b1ed31049f06acd3930a84363ff37ce774a48b7e
Josh Hackney <josh.hackney13@gmail.com>
2019-10-01 02:12:20 -0400

    creates a new vpc with subnets, defined in group_vars file

--- files/aws_vpc/main.tf
+++ files/aws_vpc/main.tf
@@ -1,4 +1,8 @@
-variable "aws_region" {}
+variable "aws_region" {
+  description = "The AWS region"
+  type = string
+  default = "us-east-1"
+}
 
 variable "base_cidr_block" {
   description = "A /16 CIDR range definition, such as 10.1.0.0/16, that the VPC will use"
@@ -8,6 +12,7 @@ variable "base_cidr_block" {
 variable "availability_zones" {
   description = "A list of availability zones in which to create subnets"
   type = list(string)
+  default = ["us-east-1a", "us-east-1b","us-east-1c","us-east-1d","us-east-1e"]
 }
 
 provider "aws" {
